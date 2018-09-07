d27e8d55653c5442db708c381505b7c14749bd1b
Ray <mrs.muneton@gmail.com>
2018-09-06 23:29:54 -0700

    Removing default variable values

--- examples/ecs-alb/s3.tf
+++ examples/ecs-alb/s3.tf
@@ -1,4 +1,31 @@
+
+
 resource "aws_s3_bucket" "s3" {
   bucket = "${var.bucket_name}"
-  acl    = "private"
+  acl    = "public-read"
+}
+
+
+resource "aws_s3_bucket_policy" "s3" {
+  bucket = "${aws_s3_bucket.s3.id}"
+  policy =<<POLICY
+{
+  "Id": "public-s3",
+  "Statement": [
+    {
+      "Sid": "public-s3",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.s3.id}/*",
+      "Principal": {
+        "AWS": [
+          "*"
+        ]
+      }
+    }
+  ]
+}
+POLICY
 }
--- examples/ecs-alb/variables.tf
+++ examples/ecs-alb/variables.tf
@@ -1,79 +1,59 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "us-west-2"
+  description = "The AWS region to create ECS and RDS."
 }
 
 variable "az_count" {
   description = "Number of AZs to cover in a given AWS region"
-  default     = "2"
 }
 
 variable "key_name" {
   description = "Name of AWS key pair"
-  default="ray.muneton.gofundme.sre"
 }
 
 variable "instance_type" {
-  default     = "t2.small"
-  description = "AWS instance type"
+  default     = "t2.micro"
 }
 
 variable "asg_min" {
   description = "Min numbers of servers in ASG"
-  default     = "1"
 }
 
 variable "asg_max" {
   description = "Max numbers of servers in ASG"
-  default     = "4"
 }
 
 variable "asg_desired" {
   description = "Desired numbers of servers in ASG"
-  default     = "2"
-}
-
-variable "admin_cidr_ingress" {
-  description = "CIDR to allow tcp/22 ingress to EC2 instance"
-  default="75.82.47.204/32"
 }
 
 variable "nginx_repository_uri" {
   description = "nginx repository url"
-  default = "491947547358.dkr.ecr.us-west-2.amazonaws.com/nginx "
 }
 
 variable "nginx_repository_arn" {
   description = "nginx repository url"
-  default = "arn:aws:ecr:us-west-2:491947547358:repository/nginx"
 }
 
 variable "nginx_tag" {
   description = "nginx image tag"
-  default     = "1.10"
 }
 
 variable "php_app_repository_arn" {
-  description = "nginx repository url"
-  default = "arn:aws:ecr:us-west-2:491947547358:repository/php-app"
+  description = "php app repository url"
 }
 
 variable "php_app_repositiry_uri" {
-  description = "nginx repository url"
-  default = "491947547358.dkr.ecr.us-west-2.amazonaws.com/php-app"
+  description = "php app repository url"
 }
 
 variable "php_app_tag" {
   description = "php app image tag"
-  default     = "7.2.9-fpm"
 }
 
 variable "bucket_name" {
   description = "S3 bucket name"
-  default     = "userpics-sre-eval"
 }
 
 variable "jumpbox_ingress_cidr" {
   description = "jumpbox ingress"
-  default = "75.82.47.204/32"
 }
